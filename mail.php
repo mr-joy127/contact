f155b4c6d70b1eca2a18daff2daeff4c1e75321a
mr-joy127 <60936401+mr-joy127@users.noreply.github.com>
2023-04-10 14:43:15 +0400

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,41 @@
+<?php 
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+$message = $_POST['user_message'];
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'prom.beton.95@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'Ghbdtnvbh90'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('prom.beton.95@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('maksimka3552@gmail.com');     // Кому будет уходить письмо 
+//$mail->addAddress('ellen@example.com');               // Nwame is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка с тестового сайта';
+$mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email. '<br>Сообщение пользователя: ' .$message;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'error';
+} else {
+    header('location: thank-you.html');
+}
+?>
